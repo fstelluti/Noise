7efa5cedf2fe2f0475b64fc17af53d56d2b61652
Anne-Marie <amsdube@gmail.com>
2015-07-15 19:26:34 -0400

    Uploading the assignment Framework (that we're going to use for the project)

---
+++ Framework/Source/Animation.cpp
@@ -0,0 +1,255 @@
+//
+// COMP 371 Assignment Framework
+//
+// Created by Nicolas Bergeron on 8/7/14.
+// Updated by Gary Chang on 14/1/15
+//
+// Copyright (c) 2014-2015 Concordia University. All rights reserved.
+//
+
+#include "Animation.h"
+#include "Renderer.h"
+#include "World.h"
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+
+// Include GLEW - OpenGL Extension Wrangler
+#include <GL/glew.h>
+
+using namespace glm;
+using namespace std;
+
+AnimationKey::AnimationKey() : Model()
+{
+}
+
+AnimationKey::~AnimationKey()
+{
+}
+
+void AnimationKey::Update(float dt)
+{
+	Model::Update(dt);
+}
+
+void AnimationKey::Draw()
+{
+	Model::Draw();
+}
+
+bool AnimationKey::ParseLine(const std::vector<ci_string> &token)
+{
+	if (token.empty())
+	{
+		return true;
+	}
+	else
+	{
+		return Model::ParseLine(token);
+	}
+}
+
+Animation::Animation() 
+	: mName(""), mCurrentTime(0.0f), mDuration(0.0f), mVertexBufferID(0), mVertexArrayID(0)
+{
+}
+
+Animation::~Animation()
+{
+}
+
+void Animation::CreateVertexBuffer()
+{
+    // This is just to display lines between the animation keys
+    for (int i=0; i<mKey.size(); ++i)
+    {
+        Vertex v;
+        v.position = mKey[i].mPosition;
+        mVertexBuffer.push_back(v);
+    }
+    
+	// Create a vertex array
+	glGenVertexArrays(1, &mVertexArrayID);
+
+	// Upload Vertex Buffer to the GPU, keep a reference to it (mVertexBufferID)
+	glGenBuffers(1, &mVertexBufferID);
+	glBindBuffer(GL_ARRAY_BUFFER, mVertexBufferID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3)*mVertexBuffer.size(), &(mVertexBuffer[0]), GL_STATIC_DRAW);
+}
+
+void Animation::Update(float dt)
+{
+	mCurrentTime += dt;
+
+    while(mCurrentTime > mDuration)
+    {
+        mCurrentTime -= mDuration;
+    }
+}
+
+void Animation::Draw()
+{
+	// Draw lines between waypoints
+	//
+	// Steps:
+	// - Draw the Vertex Buffer
+	// - The Shader used to draw the Animation is AnimationLines, only requires to send the positions of the waypoints
+	// - You need to bind the vertex buffer created in CreateVertexBuffer() as the input of the vertex shader
+	// - Also set the WorldTransform uniform variable in the vertex shader
+	// - You Should use GL_LINE_LOOP as a primitive to draw lines (not triangles)
+	// - You can look at CubeModel::Draw() for a good example!
+
+	// The Model View Projection transforms are computed in the Vertex Shader
+	glBindVertexArray(mVertexArrayID);
+	GLuint WorldMatrixLocation = glGetUniformLocation(Renderer::GetShaderProgramID(), "WorldTransform");
+    glUniformMatrix4fv(WorldMatrixLocation, 1, GL_FALSE, &mat4(1.0f)[0][0]);
+
+	// 1st attribute buffer : vertex Positions
+    glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, mVertexBufferID);
+	glVertexAttribPointer(0,
+		3,				// size
+		GL_FLOAT,		// type
+		GL_FALSE,		// normalized?
+		sizeof(Vertex), // stride
+		(void*)0        // array buffer offset
+		);
+    
+	// Draw the triangles !
+	glDrawArrays(GL_LINE_LOOP, 0, mVertexBuffer.size());
+
+	glDisableVertexAttribArray(0);
+}
+
+
+void Animation::Load(ci_istringstream& iss)
+{
+	ci_string line;
+
+	// Parse model line by line
+	while(std::getline(iss, line))
+	{
+		// Splitting line into tokens
+		ci_istringstream strstr(line);
+		istream_iterator<ci_string, char, ci_char_traits> it(strstr);
+		istream_iterator<ci_string, char, ci_char_traits> end;
+		vector<ci_string> token(it, end);
+
+		if (ParseLine(token) == false)
+		{
+			fprintf(stderr, "Error loading scene file... token:  %s!", token[0].c_str());
+			getchar();
+			exit(-1);
+		}
+	}
+}
+
+
+bool Animation::ParseLine(const std::vector<ci_string> &token)
+{
+    if (token.empty())
+    {
+        return true;
+    }
+	else if (token[0] == "name")
+	{
+		assert(token.size() > 2);
+		assert(token[1] == "=");
+
+		mName = token[2];
+		return true;
+	}
+	else if (token[0] == "key")
+	{
+		assert(token.size() > 5);
+        assert(token[1] == "=");
+        assert(token[3] == "time");
+        assert(token[4] == "=");
+
+		ci_string name = token[2];
+		AnimationKey *key = World::GetInstance()->FindAnimationKey(name);
+        
+		assert(key != nullptr);
+		AddKey(key, atof(token[5].c_str()));
+		return true;
+	}
+	return false;
+}
+
+void Animation::AddKey(AnimationKey* key, float time)
+{
+    assert(time >= mDuration);
+	mKey.push_back(*key);
+    mKeyTime.push_back(time);
+    mDuration = time;
+}
+
+ci_string Animation::GetName() const
+{
+	return mName;
+}
+
+glm::mat4 Animation::GetAnimationWorldMatrix() const
+{
+    // @TODO 3 - Find the 2 keys to interpolate the transformation
+    //           Interpolate the position, scaling and rotation separately
+    //           Finally concatenate the interpolated transforms into a single
+    //           world transform and return it.
+    
+    mat4 worldMatrix(1.0f);
+
+	//Following code retrieves the two keys
+	AnimationKey keyAfter, keyBefore;
+
+	//Used for interpolation
+	float keyTimeBefore;
+
+	//Increment is found by dividing the duration of an animation cycle by the amount of keys
+	float keyInc = mDuration / (mKey.size() - 1);
+
+	//Counter to keep track
+	int count = 0;
+
+	for(auto &v : mKeyTime) 
+	{
+		if((mCurrentTime > v) && (mCurrentTime < v + keyInc)) 
+		{
+			keyBefore = mKey.at(count); 
+			keyAfter = mKey.at(count + 1);
+			keyTimeBefore = v;
+			break;
+		}
+		
+		count++;
+	}
+	
+	//Interpolation matrices
+	mat4 interpolateAfter, interpolateBefore;
+
+	interpolateAfter = translate(interpolateAfter, keyAfter.GetPosition());
+	interpolateBefore = translate(interpolateBefore, keyBefore.GetPosition());
+
+	float interpolateProgress = abs(1-(mCurrentTime-(keyTimeBefore*keyInc))/keyInc);
+
+	if(interpolateProgress > 1)
+	{
+		interpolateProgress = interpolateProgress-1;
+	}
+
+	interpolateAfter = scale(interpolateAfter, keyAfter.GetScaling());
+	interpolateBefore = scale(interpolateBefore, keyBefore.GetScaling());
+
+	//Use mix to interpolate
+	worldMatrix = mix(interpolateAfter, interpolateBefore, interpolateProgress);
+	
+	//For rotation, we need to use quats
+	quat angleBeforeQuat = angleAxis(keyBefore.GetRotationAngle(), keyBefore.GetRotationAxis());
+	quat angleAfterQuat = angleAxis(keyAfter.GetRotationAngle(), keyAfter.GetRotationAxis());
+	quat myAngleQuat = slerp(angleBeforeQuat, angleAfterQuat, interpolateProgress);
+	
+	//Need to cast the quat as a matrix to multiply the matrices together
+	mat4 rotateMatrix = mat4_cast(myAngleQuat);
+        
+    return worldMatrix * rotateMatrix;
+}
---
+++ Framework/Source/Model.cpp
@@ -0,0 +1,165 @@
+//
+// COMP 371 Assignment Framework
+//
+// Created by Nicolas Bergeron on 8/7/14.
+// Updated by Gary Chang on 14/1/15
+//
+// Copyright (c) 2014-2015 Concordia University. All rights reserved.
+//
+
+#include "Model.h"
+#include "Animation.h"
+#include "World.h"
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/common.hpp>
+
+using namespace std;
+using namespace glm;
+
+Model::Model() 
+	: mName("UNNAMED"), mPosition(0.0f, 0.0f, 0.0f), mScaling(1.0f, 1.0f, 1.0f), mRotationAxis(0.0f, 1.0f, 0.0f), 
+	  mRotationAngleInDegrees(0.0f), mAnimation(nullptr)
+{
+}
+
+Model::~Model()
+{
+}
+
+void Model::Update(float dt)
+{
+}
+
+void Model::Draw()
+{
+}
+
+
+void Model::Load(ci_istringstream& iss)
+{
+	ci_string line;
+
+	// Parse model line by line
+	while(std::getline(iss, line))
+	{
+		// Splitting line into tokens
+		ci_istringstream strstr(line);
+		istream_iterator<ci_string, char, ci_char_traits> it(strstr);
+		istream_iterator<ci_string, char, ci_char_traits> end;
+		vector<ci_string> token(it, end);
+
+		if (ParseLine(token) == false)
+		{
+			fprintf(stderr, "Error loading scene file... token:  %s!", token[0].c_str());
+			getchar();
+			exit(-1);
+		}
+	}
+}
+
+bool Model::ParseLine(const std::vector<ci_string> &token)
+{
+		if (token.empty() == false)
+	{
+		if (token[0].empty() == false && token[0][0] == '#')
+		{
+			return true;
+		}
+		else if (token[0] == "name")
+		{
+			assert(token.size() > 2);
+			assert(token[1] == "=");
+
+			mName = token[2];	
+		}
+		else if (token[0] == "position")
+		{
+			assert(token.size() > 4);
+			assert(token[1] == "=");
+
+			mPosition.x = static_cast<float>(atof(token[2].c_str()));
+			mPosition.y = static_cast<float>(atof(token[3].c_str()));
+			mPosition.z = static_cast<float>(atof(token[4].c_str()));
+		}
+		else if (token[0] == "rotation")
+		{
+			assert(token.size() > 4);
+			assert(token[1] == "=");
+
+			mRotationAxis.x = static_cast<float>(atof(token[2].c_str()));
+			mRotationAxis.y = static_cast<float>(atof(token[3].c_str()));
+			mRotationAxis.z = static_cast<float>(atof(token[4].c_str()));
+			mRotationAngleInDegrees = static_cast<float>(atof(token[5].c_str()));
+
+			glm::normalize(mRotationAxis);
+		}
+		else if (token[0] == "scaling")
+		{
+			assert(token.size() > 4);
+			assert(token[1] == "=");
+
+			mScaling.x = static_cast<float>(atof(token[2].c_str()));
+			mScaling.y = static_cast<float>(atof(token[3].c_str()));
+			mScaling.z = static_cast<float>(atof(token[4].c_str()));
+		}
+		else if (token[0] == "animation")
+		{
+			assert(token.size() > 2);
+			assert(token[1] == "=");
+
+			ci_string animName = token[2];
+            
+            mAnimation = World::GetInstance()->FindAnimation(animName);
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+glm::mat4 Model::GetWorldMatrix() const
+{
+	// @TODO 2 - You must build the world matrix from the position, scaling and rotation informations
+    //           If the model has an animation, get the world transform from the animation.
+	mat4 worldMatrix(1.0f);
+    
+
+	//If loop checks to see if the models have an animation
+	//If they do, perform the animation by getting the AnimationWorldMatrix
+	//Else, don't animate
+	if(mAnimation == nullptr) {
+		vec3 scaleThing = GetScaling();
+		vec3 position = GetPosition();
+		vec3 rotAxis = GetRotationAxis();
+		float rotAngle = GetRotationAngle();
+		
+		//The order is actually Scale, Rotate, Translate, but the code is read in the reverse order
+		//So says the OpenGL tutorial
+		worldMatrix = translate(worldMatrix, position);
+		worldMatrix = rotate(worldMatrix, rotAngle, rotAxis);
+		worldMatrix = scale(worldMatrix, scaleThing);
+	} else {
+		worldMatrix = mAnimation->GetAnimationWorldMatrix();
+	}
+
+	return worldMatrix;
+}
+
+void Model::SetPosition(glm::vec3 position)
+{
+	mPosition = position;
+}
+
+void Model::SetScaling(glm::vec3 scaling)
+{
+	mScaling = scaling;
+}
+
+void Model::SetRotation(glm::vec3 axis, float angleDegrees)
+{
+	mRotationAxis = axis;
+	mRotationAngleInDegrees = angleDegrees;
+}
